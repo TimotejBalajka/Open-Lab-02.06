1bc2bb9d6469bffd1eefd1122c53703acff87f3b
TimotejBalajka <113421940+TimotejBalajka@users.noreply.github.com>
2022-09-27 09:16:18 +0200

    Update Calculator.cs

--- Open-Lab-02.06/Calculator.cs
+++ Open-Lab-02.06/Calculator.cs
@@ -7,9 +7,13 @@ namespace Open_Lab_02._06
         public bool Divisible(int number)
         {
             if (number % 100 == 0)
+            {
                 return true;
+            }
             else
+            {
                 return false;
+            }
         }
     }
 }
